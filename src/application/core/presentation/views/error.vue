e8f0c325178a41c96b202174dfc227c48dc3b9e0
andrew <a@fandb.pro>
2025-04-20 17:13:03 +0300

    feat: error page tidy up

--- src/application/core/presentation/views/error.vue
+++ src/application/core/presentation/views/error.vue
@@ -1,29 +1,64 @@
 <script setup lang="ts">
-defineOptions({
-  name: 'ErrorComponent',
-});
 import { useRoute } from 'vue-router';
 
 import { COCKTAILS } from '@/application/cocktails/domain/types.ts';
 import { ROUTES } from '@/application/core/domain/routes.ts';
+import AppFooter from '@/application/core/presentation/components/AppFooter.vue';
+
+defineOptions({
+  name: 'ErrorComponent',
+});
 
 const route = useRoute();
 </script>
 
 <template>
-  <p>Error 404: Page Not Found</p>
+  <div :class="s.wrapper">
+    <h1>Error 404: Page Not Found</h1>
 
-  <p v-if="!route.params.pathMatch">
-    There is no
-    <code>{{ route.path }}</code>
-    route
-  </p>
-  <p>Try one of these:</p>
-  <ul>
-    <li v-for="slug in COCKTAILS" :key="slug">
-      <RouterLink :to="{ name: ROUTES.cocktailsShow.name, params: { slug } }">
-        {{ slug }}
-      </RouterLink>
-    </li>
-  </ul>
+    <p v-if="!route.params.pathMatch">
+      There is no
+      <code>{{ route.path }}</code>
+      route
+    </p>
+    <p>Try one of these:</p>
+    <ul :class="s.slugWrapper">
+      <li v-for="slug in COCKTAILS" :key="slug" :class="s.slugItem">
+        <RouterLink :to="{ name: ROUTES.cocktailsShow.name, params: { slug } }">
+          {{ slug }}
+        </RouterLink>
+      </li>
+    </ul>
+
+    <AppFooter />
+  </div>
 </template>
+
+<style module="s">
+.wrapper {
+  display: flex;
+  height: calc(100vh - var(--footer-height));
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  overflow: hidden;
+}
+
+.slugWrapper {
+  display: flex;
+  gap: 0.5rem;
+}
+
+.slugItem {
+  list-style-type: none;
+
+  a {
+    color: var(--color-link);
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+}
+</style>
