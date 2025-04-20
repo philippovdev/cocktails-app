d1ea8f28fda56c25dfc4faf68fd14f8cbc2ff0c5
andrew <a@fandb.pro>
2025-04-20 22:08:45 +0300

    chore: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
     },
-  },
+  };
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,21 @@
 import { fileURLToPath } from 'node:url';
 
-import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
+import { mergeConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(async () => {
+  const viteConfigResolved = viteConfig({
+    mode: 'test',
+    command: 'build',
+  });
+
+  return mergeConfig(viteConfigResolved, {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  })
-);
+  });
+});
